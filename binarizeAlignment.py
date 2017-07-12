d7e474401b8dc2c1063c8fe7b967035ec0b94f2c
Adriana <adriana.sperlea@gmail.com>
2017-07-11 18:43:06 -0700

    binarizeAlignment.py now more user friendly as well

--- binarizeAlignment.py
+++ binarizeAlignment.py
@@ -27,37 +27,26 @@ def createFeatures(splitLine):
     return int(splitLine[0]), features
 
 def main():
-    if len(sys.argv) < 6:
-        print "Usage: binarizeAlignment.py <MAF features directory> <output directory> <alignment name> <chromosome> <lines per chunk>"
+    if len(sys.argv) < 5:
+        print "Usage: binarizeAlignment.py <MAF sequence file> <output directory> <chromosome> <lines per chunk> <chromosome lengths file>"
         exit(1)
 
-    featuresDir = formatDir(sys.argv[1])
+    alignmentFile = gzip.open(sys.argv[1], 'r')
     oDir = formatDir(sys.argv[2])
-    alignmentName = sys.argv[3]
-    chr = sys.argv[4]
-    linesPerChunk = int(sys.argv[5])
+    chr = sys.argv[3]
+    linesPerChunk = int(sys.argv[4])
+    chromLengths = sys.argv[5]
 
-    chrLength = {"chr1":249250621, "chr2": 243199373, "chr3": 198022430, "chr4": 191154276, "chr5": 180915260,
-            "chr6": 171115067, "chr7": 159138663, "chr8": 146364022, "chr9": 141213431, "chr10": 135534747,
-            "chr11": 135006516, "chr12": 133851895, "chr13": 115169878, "chr14": 107349540, "chr15": 102531392,
-            "chr16": 90354753, "chr17": 81195210, "chr18": 78077248, "chr19": 59128983, "chr20": 63025520,
-            "chr21": 48129895, "chr22": 51304566, "chrX": 155270560, "chrY": 59373566}
+    chrLength = {}
+    for line in chromSizes:
+        splitLine = line.strip().split()
+        chrLength[splitLine[0]] = int(splitLine[1])
+
     totalGenomeLength = sum(chrLength.values())
-
-    if alignmentName == "UCSC":
-        numFeats = 99
-    elif alignmentName == "Ensembl_EPO":
-        numFeats = 36
-    elif alignmentName == "Ensembl_Pecan":
-        numFeats = 20
-    else:
-        print "Alignment name not recognized."
-        exit(1)
-
-    alignmentFile = gzip.open(featuresDir + chr + "_features.out.gz", 'r')
 
     # Read file headers to get them out of the way
     header = alignmentFile.readline()
+    numFeats = len(header.split(",")) - 2
 
     startTime = time.time()
     curChunk = 0
