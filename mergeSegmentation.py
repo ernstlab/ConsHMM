b8b2d979be0d75fcdd4166fec590f410adb560f6
Adriana <adriana.sperlea@gmail.com>
2017-10-24 13:33:34 -0700

    Updated mergeSegmentation.py to simpler format

--- mergeSegmentation.py
+++ mergeSegmentation.py
@@ -1,29 +1,32 @@
 from __future__ import print_function
 import glob
 import sys
+import gzip
 from shared import *
 
 def main():
-    if len(sys.argv) < 5:
-        print("Usage: python mergeSegmentation.py <bed folder> <output file> <segment size> <num states>")
+    if len(sys.argv) < 6:
+        print("Usage: python mergeSegmentation.py <bed folder> <output file> <segment size> <num states> <suffix include>")
         exit(1)
 
     bedFolder = formatDir(sys.argv[1])
-    oFile = open(sys.argv[2], 'w')
+    oFile = gzip.open(sys.argv[2], 'w')
     segmentSize = int(sys.argv[3])
     numStates = sys.argv[4]
+    suffix = sys.argv[5]
 
     offset = 0
-    fileNames = glob.glob(bedFolder + '*_segments.bed')
+    fileNames = glob.glob(bedFolder + '*_' + suffix + '_segments.bed')
     maxIdx = 0
     for fileName in fileNames:
-        idx = int(fileName.split("_")[-3].split("cell")[-1])
+        idx = int(fileName.split("_")[-4].split("cell")[-1])
         if idx > maxIdx:
             maxIdx = idx
 
     lastLine = ""
     for idx in range(0, maxIdx + 1):
-        file = bedFolder + "cell" + str(idx) + "_" + numStates + "_segments.bed"
+        print("idx = ", idx)
+        file = bedFolder + "cell" + str(idx) + "_" + numStates + "_" + suffix + "_segments.bed"
         print("Joining file ", file)
 
         bedFile = open(file, 'r')
@@ -32,21 +35,22 @@ def main():
             splitLine = line.split("\t")
             splitLine[1] = str(int(splitLine[1]) + offset)
             splitLine[2] = str(int(splitLine[2]) + offset)
-            if firstLine:
+            
+            if lastLine != "":
                 if lastLine[0] == splitLine[0]: # if on the same chromosome
-                    if lastLine[-1] == splitLine[-1]: #if in the same state
-                        oFile.write(lastLine[0] + "\t" + lastLine[1] + "\t" + splitLine[2] + "\t" + splitLine[-1]) # join them
+                    if lastLine[-1] == splitLine[-1]: # if in the same state
+                        lastLine[2] = splitLine[2]
                     else:
-                        oFile.write("\t".join(lastLine)) # write last line separately
-                        oFile.write("\t".join(splitLine)) # write first line separately
+                        oFile.write("\t".join(lastLine))
+                        lastLine = splitLine
                 else:
-                    oFile.write("\t").join(splitLine) # write last line separately
-                    oFile.write("\t".join(splitLine)) # write first line separately
-                firstLine = False
+                    oFile.write("\t".join(lastLine))
+                    lastLine = splitLine
             else:
-                oFile.write("\t".join(splitLine)) # if not the first line always just write the line
                 lastLine = splitLine
         offset += segmentSize
+
+    oFile.write("\t".join(lastLine))
     oFile.close()
 
 main()
